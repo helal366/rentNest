1039ca78b5d2e754339113413eb8c49f2e82b0dd
Helal366 <helal56876@gmail.com>
2026-07-10 02:55:57 +0600

    created a small html for backend welcome page

--- src/app.ts
+++ src/app.ts
@@ -23,8 +23,22 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+const html = `<html>
+      <head>
+        <title>rentNest</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body class="bg-gray-200">
+        <div class="my-10 p-10 max-w-xl mx-auto text-center bg-gray-100 shadow-2xl shadow-[#98CD0090] rounded">
+          <h1 class="text-3xl font-bold text-teal-600 mb-4">rentNest</h1>
+          <p class="text-lg text-gray-800 mb-2">🍽️ Welcome to rentNest!</p>
+          <p class="text-lg text-gray-800 mb-2">🍲 Find your sweet home!</p>
+          <p class="text-lg text-gray-700 mb-2">🍽️ Explore for your dignity. Search Office, Shop or others.!</p>
+        </div>
+      </body>
+    </html>`
 app.get("/", (req: Request, res: Response) => {
-  res.send("This is a web site to rent a nice nest for you and your family.");
+  res.send(html);
 });
 
 app.use("/api/auth", authRouter);
