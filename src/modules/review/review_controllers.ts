52013f032542163c20e5a795ffd340fa7b79a867
Helal366 <helal56876@gmail.com>
2026-07-12 16:10:45 +0600

    review routes banned check business logic updated

--- src/modules/review/review_controllers.ts
+++ src/modules/review/review_controllers.ts
@@ -4,13 +4,14 @@ import { AppError } from "../../utils/globalErrorHelper.js";
 import { StatusCodes } from "http-status-codes";
 import { reviewServices } from "./review_services.js";
 import { sendResponse } from "../../utils/sendResponse.js";
+import { userCheck } from "../../utils/userCheck.js";
 
 const createReviewController = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     if(!req.user){
         throw new AppError("Please login", StatusCodes.UNAUTHORIZED)
     }
-
+    userCheck(req.user)
     const payload = {
       tenantId: req.user.id,
       tenantRole: req.user.role,
